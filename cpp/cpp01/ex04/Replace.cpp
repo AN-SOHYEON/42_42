8aaa1506148e20a91b97c42f6d23a355a44bd687
anso33 <hyeon131812@gmail.com>
2022-07-29 08:03:36 +0900

    fin 01-04

--- cpp/cpp01/ex04/Replace.cpp
+++ cpp/cpp01/ex04/Replace.cpp
@@ -2,20 +2,22 @@
 
 int	inputFile(std::string& content, const char* filename) 
 {
-	std::ifstream	in(filename);
+	std::ifstream	in(filename, std::ifstream::in);
 	std::string		line;
 
 	if (!in.is_open()) 
 	{
-		std::cerr << "faile to open file" << std::endl;
-		return (1);
+		std::cout << "fail to open file" << std::endl;
+		return (0);
 	}
-	while (std::getline(in, line))
+	while (!in.eof())
 	{
+		std::getline(in, line);
 		content = content + line;
 		content.push_back('\n');
 	}
-	return (0);
+	in.close();
+	return (1);
 }
 
 int	outputFile(const std::string& content, const char* filename)
@@ -23,23 +25,24 @@ int	outputFile(const std::string& content, const char* filename)
 	std::string		filename_re;
 	filename_re = std::string(filename) + ".replace";
 
-	std::ofstream	out(filename_re.c_str());
+	std::ofstream	out(filename_re.c_str(), std::ofstream::out);
 	if (!out.is_open())
 	{
-		std::cerr << "faile to open new file." << std::endl;
-		return (1);
+		std::cout << "fail to open replace file." << std::endl;
+		return (0);
 	}
 	out << content;
-	return (0);
+	out.close();
+	return (1);
 }
 
 int replace(const char* filename, const char* search, const char* replace)
 {
-	std::string content;
-	std::size_t i;
+	std::string	content;
+	std::size_t	i;
 
 	if (!inputFile(content, filename))
-		return false;
+		return (0);
 	i = 0;
 	while (1)
 	{
@@ -51,6 +54,6 @@ int replace(const char* filename, const char* search, const char* replace)
 		i  = i + strlen(replace);
 	}
 	if (!outputFile(content, filename))
-		return false;
-	return true;
+		return (0);
+	return (1);
 }
