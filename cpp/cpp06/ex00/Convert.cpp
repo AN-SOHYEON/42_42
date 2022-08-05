d3e66de4b11699e4f58e23c58ab3c45f4d388388
anso33 <hyeon131812@gmail.com>
2022-08-05 15:44:49 +0900

    06-ing | add 42Header

--- cpp/cpp06/ex00/Convert.cpp
+++ cpp/cpp06/ex00/Convert.cpp
@@ -1,23 +1,100 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Convert.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: san <san@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/05 14:58:35 by san               #+#    #+#             */
+/*   Updated: 2022/08/05 14:58:36 by san              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 # include "Convert.hpp"
 
-class Cast
-{
-private:
-	/* data */
-public:
-	Cast(/* args */);
-	~Cast();
-	char	identify(std::string value);
 
+
+
+class Convert
+{
+	private:
+		char		type;
+		std::string	value;
+	public:
+		Convert();
+		Convert(char type, std::string value);
+		Convert(const Convert &convert);
+		~Convert();
+		Convert	&operator=(const Convert &convert);
+
+		void	conversion();
+		void	convertChar();
+		void	convertInt();
+		void	convertFloat();
+		void	convertDouble();
+		
+	class	NoType : public std::exception
+	{
+		public :
+			const char	*what() const throw();
+	};
 
 };
 
-Cast::Cast(/* args */)
+void	Convert::convertChar()
+{
+
+}
+
+void	Convert::convertInt()
+{
+
+}
+
+void	Convert::convertFloat()
+{
+
+}
+
+void	Convert::convertDouble()
+{
+
+}
+
+
+void	Convert::conversion()
+{
+	switch (this->type)
+	{
+		case ('c'):
+			convertChar();
+			break;
+		case ('i'):
+			convertInt();
+			break;
+		case ('f'):
+			convertFloat();
+			break;
+		case ('d'):
+			convertDouble();
+			break;
+		default : 
+			throw (NoType());
+	}
+}
+
+
+
+
+Convert::Convert(/* args */)
 {
 }
 
-Cast::~Cast()
+Convert::Convert(char type, std::string value)
+{
+
+}
+
+Convert::~Convert()
 {
 }
--- cpp/cpp06/ex00/main.cpp
+++ cpp/cpp06/ex00/main.cpp
@@ -1,6 +1,42 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: san <san@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/05 14:58:53 by san               #+#    #+#             */
+/*   Updated: 2022/08/05 14:58:56 by san              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "Convert.hpp"
+
+char	findType(std::string value)
+{
+	int	len = value.length();
+	
+	switch (len)
+	{
+		case (0):
+			return (NULL);
+		case (1):
+			if (value[0] > 32 || value[0] < 127)
+				return ('c');
+			else 
+				return ('i');
+		default :
+			if (value.find(".") < 0)
+				return ('i');
+			else
+			{
+				if (value.find("f") < 0)
+					return ('d');
+				else
+					return ('f');
+			}
+	}
+}
 
 int	main(int ac, char **av)
 {
@@ -10,7 +46,7 @@ int	main(int ac, char **av)
 		return (1);
 	}
 	std::string	value = av[1];
-
+	Convert convert(findType(value), value);
 
 
 	return (0);
