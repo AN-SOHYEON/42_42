69365e591b4bb198731816380273a4a00eb874ba
anso33 <hyeon131812@gmail.com>
2023-01-24 16:21:54 +0900

    feat: -ing implementing

--- ft_container/includes/map.hpp
+++ ft_container/includes/map.hpp
@@ -13,16 +13,16 @@
 
 namespace ft
 {
-	// template <
-	// 	class Key,
-	// 	class T,
-	// 	class Compare = std::less<Key>, // map의 정렬 기준 조건자
-	// 	class Allocator = std::allocator<ft::pair<const Key, T> > >
 	template <
 		class Key,
 		class T,
-		class Compare,
-		class Allocator>
+		class Compare = std::less<Key>, // map의 정렬 기준 조건자
+		class Allocator = std::allocator<ft::pair<const Key, T> > >
+	// template <
+	// 	class Key,
+	// 	class T,
+	// 	class Compare,
+	// 	class Allocator>
 	class map
 	{
 
@@ -34,7 +34,8 @@ namespace ft
 		typedef typename std::ptrdiff_t difference_type;
 		typedef Compare key_compare;
 		typedef Allocator allocator_type;
-		typedef typename allocator_type::template rebind<ft::node<value_type> >::other node_allocator;
+		typedef typename allocator_type::template rebind<ft::Node<value_type> >::other node_allocator;
+		// typedef typename allocator_type::template rebind<ft::node<value_type> >::other node_allocator;
 		// TODO: rebind<ft::avlTree>
 
 		typedef value_type &reference;
@@ -52,6 +53,7 @@ namespace ft
 		class value_compare : public std::binary_function<value_type, value_type, bool>
 		{
 			friend class map;
+			// map 클래스의 모든 멤버 함수는 value_compare 클래스에 대한 프렌드 접근 권한을 부여받는다.
 
 		protected:
 			key_compare comp;
@@ -64,11 +66,31 @@ namespace ft
 			}
 		};
 
-	private:
-		tree *_tree;
-		node_allocator _alloc;
+	protected:
+		tree _tree;
+		node_allocator _alloc; // TODO: 둘지말지 고민하기
 		key_compare _key_comp;
 		value_compare _value_comp;
+
+		template <class InputIterator> // node 기반 컨테이너는 이터레이터간의 거리를 뺄셈으로 구할 수 없다.
+		typename iterator_traits<InputIterator>::difference_type _distance(InputIterator first, InputIterator last)
+		{
+			typename iterator_traits<InputIterator>::difference_type n = 0;
+			while (first != last)
+			{
+				first++;
+				n++;
+			}
+			return (n);
+		}
+
+		template <typename U>
+		void _swap(U &a, U &b)
+		{
+			U tmp = a;
+			a = b;
+			b = tmp;
+		}
 
 	public:
 		map()
@@ -93,7 +115,7 @@ namespace ft
 			const Compare &comp = Compare(),
 			const Allocator &alloc = Allocator())
 		{
-
+			_tree;			// TODO: tree 처리
 			_alloc = alloc; // TODO: 아ㅏㅏㅏ allocator 모르겠는디ㅣㅣㅣ
 			_key_comp = comp;
 			_value_comp = value_compare(_key_comp);
@@ -103,7 +125,14 @@ namespace ft
 
 		virtual ~map() {}
 
-		map &operator=(const map &other);
+		map &operator=(const map &other)
+		{
+			_tree = other._tree;
+			_alloc = other._alloc;
+			_key_comp = other._key_comp;
+			_value_comp = other._value_comp;
+			return (*this);
+		}
 
 		allocator_type get_allocator() const
 		{
@@ -112,37 +141,62 @@ namespace ft
 
 		mapped_type &at(const key_type &key)
 		{
-			if (_tree->findTree(key))
-				throw std::out_of_range("map");
-			return _tree->findTree(key)->second; // TODO: 음 pair일 때 어케 find?
+			if (_tree.findTree(key))
+				throw std::out_of_range("map over");
+			return _tree.findTree(key)->second; // TODO: 음 pair일 때 어케 find?
 		}
 
 		const mapped_type &at(const key_type &key) const
 		{
-			if (_tree->findTree(key))
-				throw std::out_of_range("map");
-			return _tree->findTree(key)->second; // TODO: 음 pair일 때 어케 find?
+			if (_tree.findTree(key))
+				throw std::out_of_range("map over");
+			return _tree.findTree(key)->second; // TODO: 음 pair일 때 어케 find?
 		}
 
-		T &operator[](const Key &key);
+		T &operator[](const Key &key)
+		{
+			return _tree.findTree(key)->second;
+		}
 
 		/*
 		iterator:	TODO: implement
 		 */
 		iterator begin()
 		{
-			// return iterator()
+			return iterator(_tree.begin_node());
 		}
-		const_iterator begin() const;
+		const_iterator begin() const
+		{
+			return const_iterator(_tree.end_node());
+		}
 
-		iterator end();
-		const_iterator end() const;
+		iterator end()
+		{
+			return iterator(_tree.end_node()); // TODO: iterator
+		}
+		const_iterator end() const
+		{
+			return const_iterator(_tree.end_node());
+		}
 
-		reverse_iterator rbegin();
-		const_reverse_iterator rbegin() const;
+		reverse_iterator rbegin()
+		{
+			return reverse_iterator(_tree.end_node());
+		}
+		const_reverse_iterator rbegin() const
+		{
+			return const_reverse_iterator(_tree.end_node());
+		}
 
-		reverse_iterator rend();
-		const_reverse_iterator rend() const;
+		reverse_iterator rend()
+		{
+			return reverse_iterator(_tree.end_node());
+		}
+
+		const_reverse_iterator rend() const
+		{
+			return const_reverse_iterator(_tree.end_node());
+		}
 
 		/*
 		capacity:
@@ -156,7 +210,7 @@ namespace ft
 
 		size_type size() const
 		{
-			return std::distance(begin(), end()); // TODO: distance
+			return _distance(begin(), end()); // TODO: distance
 		}
 
 		size_type max_size() const
@@ -165,7 +219,7 @@ namespace ft
 		}
 
 		/*
-		modifiers
+		modifiers:
 		 */
 		// 모든 원소 제거
 		void clear();
@@ -177,10 +231,16 @@ namespace ft
 		void erase(iterator first, iterator last);
 		size_type erase(const Key &key);
 
-		void swap(map &other);
+		void swap(map &other)
+		{
+			_swap(_tree, other._tree);
+			_swap(_alloc, other._alloc);
+			_swap(_key_comp, other._key_comp);
+			_swap(_value_comp, other._value_comp);
+		}
 
 		/*
-		Lookup
+		Lookup:
 		 */
 		size_type count(const Key &key) const;
 
@@ -200,7 +260,7 @@ namespace ft
 		const_iterator upper_bound(const Key &key) const;
 
 		/*
-		Observers
+		Observers:
 		 */
 		// key 정렬 기준인 조건자를 반환
 		key_compare key_comp() const
